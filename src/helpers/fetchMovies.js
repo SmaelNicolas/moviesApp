c8219af8b0667188d3afbd489c057102c5f5d5df
Smael Nicolas <nico.smael@gmail.com>
2022-01-23 21:09:14 -0300

    trending , images , posters , genre

--- src/helpers/fetchMovies.js
+++ src/helpers/fetchMovies.js
@@ -2,7 +2,14 @@ const logg = () => {
 	console.log("Hola");
 
 	fetch(
-		"https://api.themoviedb.org/3/discover/movie?api_key=c9c649f8dc1856154cf88a683b935a6e"
+		//Lista de generos
+		// "https://api.themoviedb.org/3/genre/movie/list?api_key=c9c649f8dc1856154cf88a683b935a6e"
+
+		//una pelicula en particular
+		// "https://api.themoviedb.org/3/discover/movie?api_key=c9c649f8dc1856154cf88a683b935a6e"
+
+		//trending  /trending/{media_type}/{time_window}
+		"https://api.themoviedb.org/3/trending/movie/week?api_key=c9c649f8dc1856154cf88a683b935a6e"
 	)
 		.then((data) => {
 			return data.json();
@@ -10,9 +17,9 @@ const logg = () => {
 		.then((data) => {
 			console.log(data);
 			//IMAGEN
-			console.log(`https://image.tmdb.org/t/p/w500${data.backdrop_path}`);
+			// console.log(`https://image.tmdb.org/t/p/w500${data.backdrop_path}`);
 			//POSTER
-			console.log(`https://image.tmdb.org/t/p/w500${data.poster_path}`);
+			// console.log(`https://image.tmdb.org/t/p/w500${data.poster_path}`);
 			//
 		});
 };

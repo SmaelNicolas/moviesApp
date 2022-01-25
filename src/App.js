6750d9b1a002a624b903ea405b332764cde409b0
Smael Nicolas <nico.smael@gmail.com>
2022-01-25 20:30:58 -0300

    delets imports

--- src/App.js
+++ src/App.js
@@ -1,60 +1,50 @@
 import GlobalStyle from "./globalStyled";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { IsLoggedInContext } from "./context/IsLoggedContext";
 
-import Fetch from "./helpers/Fetch";
 import Navbar from "./Navbar/Navbar";
 import Browse from "./pages/Browse/Browse";
-import Latest from "./pages/Latest/Latest";
 import Login from "./pages/Login/Login";
 import MyList from "./pages/MyList/MyList";
 import WhosWatching from "./pages/WhosWatching/WhosWatching";
-import { useEffect, useState } from "react";
+import Movies from "./pages/Movies/Movies";
+import Series from "./pages/Series/Series";
 
 function App() {
-	const [isLogged, setIsLogged] = useState(false);
+	const [isLogged] = useContext(IsLoggedInContext);
 	return (
-		<BrowserRouter>
-			<Navbar />
+		<>
 			<GlobalStyle />
-			{useEffect(() => {
-				setTimeout(() => {
-					// setIsLogged(true);
-				}, 2000);
-			}, [isLogged])}
-			{isLogged ? (
-				<>
-					<Fetch />
+			<BrowserRouter>
+				{isLogged ? (
+					<>
+						{/* <Fetch /> */}
+						<Navbar />
+						<Routes>
+							<Route
+								exact
+								path='/profiles'
+								element={<WhosWatching />}
+							/>
+							<Route exact path='/' element={<Browse />} />
+							<Route exact path='/series' element={<Series />} />
+							<Route exact path='/movies' element={<Movies />} />
+
+							<Route
+								exact
+								path='/browse/mylist'
+								element={<MyList />}
+							/>
+						</Routes>
+					</>
+				) : (
 					<Routes>
-						<Route
-							exact
-							path='/profiles'
-							element={<WhosWatching />}
-						/>
-						<Route exact path='/' element={<Browse />} />
-						<Route
-							exact
-							path='/browse/series'
-							element={<Browse />}
-						/>
-						<Route
-							exact
-							path='/browse/movies'
-							element={<Browse />}
-						/>
-						<Route exact path='/latest' element={<Latest />} />
-						<Route
-							exact
-							path='/browse/mylist'
-							element={<MyList />}
-						/>
+						<Route exact path='/' element={<Login />} />
 					</Routes>
-				</>
-			) : (
-				<Routes>
-					<Route exact path='/' element={<Login />} />
-				</Routes>
-			)}
-		</BrowserRouter>
+				)}
+			</BrowserRouter>
+		</>
 	);
 }
 
